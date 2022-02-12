d462c903a7af91a86b2734c13e945cee19a3f9ef
Alexander Johansson <alexander.og.johansson@hotmail.com>
2022-02-12 22:28:18 +0100

    added overlaps for player and sprites

--- core/level.py
+++ core/level.py
@@ -42,23 +42,21 @@ class Level:
 class YSortCameraGroup(pygame.sprite.Group):
 
     def __init__(self):
-
         #general setup
         super().__init__()
         self.display_surface = pygame.display.get_surface()
         self.offset = pygame.math.Vector2()
-
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
 
 
     def custom_draw(self, player):
-
         # Getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
-        for sprite in self.sprites():
+        # for sprite in self.sprites():
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image,offset_pos)
 
